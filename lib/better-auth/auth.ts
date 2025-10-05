c2cd67d330b5c41a6224c189634ba1223e14ad15
Evan <evanz1215@gmail.com>
2025-10-06 05:57:24 +0800

    Refactor code structure for improved readability and maintainability

---
+++ lib/better-auth/auth.ts
@@ -0,0 +1,42 @@
+import { connectToDatabase } from "./../../database/mongoose";
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
+
+let authInstance: ReturnType<typeof betterAuth> | null = null;
+
+// export const auth = betterAuth({
+
+// });
+
+export const getAuth = async () => {
+  if (authInstance) {
+    return authInstance;
+  }
+
+  const mongoose = await connectToDatabase();
+  const db = mongoose.connection.db;
+
+  if (!db) {
+    throw new Error("MongoDB connection not found");
+  }
+
+  authInstance = betterAuth({
+    database: mongodbAdapter(db as any),
+    secret: process.env.BETTER_AUTH_SECRET!,
+    baseURL: process.env.BETTER_AUTH_URL!,
+    emailAndPassword: {
+      enabled: true,
+      disableSignUp: false, // Disable sign-ups
+      requireEmailVerification: false, // Require email verification
+      minPasswordLength: 8, // Minimum password length
+      maxPasswordLength: 128, // Maximum password length
+      autoSignIn: true, // Automatically sign in after sign-up
+    },
+    plugins: [nextCookies()],
+  });
+
+  return authInstance;
+};
+
+export const auth = await getAuth();
